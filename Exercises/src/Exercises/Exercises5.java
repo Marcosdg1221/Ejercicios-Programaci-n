a22a8737985fdda413e8b491c9dc70028751104e
Marcosdg1221 <114072151+Marcosdg1221@users.noreply.github.com>
2022-09-26 09:54:44 +0200

    Update Exercises5.java

--- Exercises/src/Exercises/Exercises5.java
+++ Exercises/src/Exercises/Exercises5.java
@@ -4,17 +4,24 @@ import java.util.Scanner;
 
 public class Exercises5 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner al = new Scanner(System.in);
+		//Pido al usuario un número
 		System.out.print("Introduce un número");
+		//Creo la variable
 		float numerone;
-		numerone = sc.nextFloat();
+		//El valor será lo que escriba el usuario
+		numerone = al.nextFloat();
+		//Pido al usuario que escriba otro 	número
 		System.out.print("Introduce otro");
+		//Hago lo mismo con;
 		float numerdos;
-		numerdos = sc.nextFloat();
+		numerdos = al.nextFloat();
+		//Creo dos variables que hagan las operaciones
 		float suma = (numerone + numerdos);
 		float resta = (numerone - numerdos);
 		float multi = (numerone * numerdos);
 		float divi = (numerone / numerdos);
+		//Muestro los resultados
 		System.out.println("La suma de esos dos numeros es " + suma);
 		System.out.println("La resta de esos dos numeros es " + resta);
 		System.out.println("La multiplicación de esos dos numeros es " + multi);
