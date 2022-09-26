451d6e40c709a2653e8d71b97244ff1151eb430b
Marcosdg1221 <114072151+Marcosdg1221@users.noreply.github.com>
2022-09-26 10:02:05 +0200

    Update Exercises6.java

--- Exercises/src/Exercises/Exercises6.java
+++ Exercises/src/Exercises/Exercises6.java
@@ -5,17 +5,27 @@ import java.util.Scanner;
 public class Exercises6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		//Pido al usuario su nombre;
 		System.out.print("Introduce tu nombre");
+		//Creo la variable de String para la cadena de caracteres
 		String nombre;
+		//Lo que escriba el usuario será su valor
 		nombre = sc.nextLine();
+		//Pido su dirección
 		System.out.print("Introduce tu dirección");
+		//Creo la variable de String para la cadena de caracteres
 		String direccion;
+		//Lo que escriba el usuario será su valor
 		direccion = sc.nextLine();
+		//Pido su nº teléfono 
 		System.out.print("Introduce tu telefono");
+		//creo el valor numérico para el teléfono
 		int telefono;
+		//Lo que escriba el usuario será su valor
 		telefono = sc.nextInt();
+		//Muestro los resultados
 		System.out.println("Nombre: " + nombre);
 		System.out.println("Dirección: " + direccion);
-		System.out.println("Nombre: " + telefono); //comentario
+		System.out.println("Nombre: " + telefono);
 }
 }	
